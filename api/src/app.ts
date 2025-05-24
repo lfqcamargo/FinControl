d1586b06bd8e6d954358b83b0dbc5e1bdf2e2bd3
Lucas Camargo <lfqcamargo@gmail.com>
2025-05-23 22:06:05 -0300

    Adicionado refresh token

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,4 +1,5 @@
 import fastify from "fastify";
+import fastifyCookie from "@fastify/cookie";
 import { userRoutes } from "@/http/routes/user-routes";
 import { ZodError } from "zod";
 import { env } from "@/env";
@@ -8,7 +9,16 @@ export const app = fastify();
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: "refreshToken",
+    signed: false,
+  },
+  sign: {
+    expiresIn: "10m",
+  },
 });
+app.register(fastifyCookie);
+
 app.register(userRoutes);
 
 app.setErrorHandler((error, _, reply) => {
--- api/src/http/routes/user-routes.ts
+++ api/src/http/routes/user-routes.ts
@@ -3,10 +3,13 @@ import { createUserController } from "../controllers/users/create-user-controlle
 import { authenticateController } from "../controllers/users/authenticate-controller";
 import { getUserProfileController } from "../controllers/users/get-user-profile-controller";
 import { verifyJwt } from "@/middlewares/verify-jwt";
+import { refreshTokenController } from "../controllers/users/refresh-token-controller";
 
 export async function userRoutes(app: FastifyInstance) {
   app.post("/users", createUserController);
   app.post("/sessions", authenticateController);
+
+  app.patch("/token/refresh", refreshTokenController);
 
   app.get("/profile", { onRequest: [verifyJwt] }, getUserProfileController);
 }
